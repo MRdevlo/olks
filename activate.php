35abb656b71624ed7d2e81b5dd56e496df9316b3
root <root@together-gaming.de>
2016-05-08 02:13:41 +0200

    delete all

--- activate.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_POST["key"]) and isset($_POST["hash"])){
-require_once(__DIR__."/assets/class/function.php");
-$func = new func();
-echo $func->aktiv($_POST["key"], $_POST["hash"]);
-}
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_SESSION))
-{session_start();}
-session_destroy();
-echo "Sie wurden abgemeldez";
-header('Location:index.php');
-exit();
-?>
