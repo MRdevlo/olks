0a9f541052e634e0d4cbb22dd9370aecaf52dfef
root <root@together-gaming.de>
2016-05-10 07:11:56 +0200

    V 0.2

--- config.php
+++ config.php
@@ -1,9 +1,3 @@
 <?php
-$config["dbuser"] = "olks";
-$config["dbpassword"] = "EuTyUA4LXVmCMvcFeqYB6QoHWnbg9751D02JN3hstpZlakrSjOIdPfGi8zxKRw";
-$config["dbname"] = "olks";
-$config["dbprefix"] = "olks";
-$config["dbserver"] ="localhost";
-$config["lang"] = 'en';
 $config["install"] = true;
 ?>
